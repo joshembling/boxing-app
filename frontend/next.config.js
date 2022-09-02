4424e215dee382da5959aa6efd091c970d211044
joshembling <65712975+joshembling@users.noreply.github.com>
2022-09-02 17:30:04 +0100

    another try to bypass CORS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,9 +1,33 @@
-module.exports = {
-    async rewrites() {
+const moduleExports = {
+    async headers() {
         return [
             {
-                source: '/api/:path*',
-                destination: 'https://boxing.joshembling.co.uk/:path*',
+                source: '/api/(.*)',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: 'https://boxing.joshembling.co.uk',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value:
+                            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
+            },
+        ]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/account',
+                destination: '/account/profile',
+                permanent: true,
             },
         ]
     },
